01dcf59a4a830e934320772eeed7e8111618d266
equaliser0 <accounts@equaliser.minimalcity.de>
2022-07-24 15:16:08 +0200

    facelift

--- src/components/PillTag.vue
+++ src/components/PillTag.vue
@@ -24,7 +24,6 @@ const componentClass = computed(() => {
   const baseColor = props.outline ? colorsOutline[props.type] : [colorsBg[props.type], colorsBorders[props.type]]
 
   return [
-    'border',
     props.small ? 'py-0.5 px-2 text-xs rounded-lg mr-1.5' : 'py-2 px-4 rounded-2xl mr-3',
     ...baseColor
   ]
